061b403d6205e6bbeb399c70c9a27c03c8ed446a
WaiWaiAung33 <nawtoo1997@gmail.com>
2020-07-22 10:58:06 -0800

    [WWA]

---
+++ src/components/CustomerCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default class Card extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.dateText}>{this.props.date}</Text>
+        <View style={styles.secondContainer}>
+          <View style={{ width: "30%" }}>
+            <Text style={[styles.text, { fontWeight: "bold" }]}>Name</Text>
+            <Text style={[styles.text, { fontWeight: "bold" }]}>Phone No</Text>
+            <Text style={[styles.text, { fontWeight: "bold" }]}>NRC</Text>
+            <Text style={[styles.text, { fontWeight: "bold" }]}>Address</Text>
+          </View>
+          <View style={{ width: "70%" }}>
+            <Text style={styles.text}>{this.props.name}</Text>
+            <Text style={styles.text}>{this.props.phone}</Text>
+            <Text style={styles.text}>{this.props.nrc}</Text>
+            <Text style={styles.text}>{this.props.address}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: "#707070",
+    padding: 10,
+  },
+  text: {
+    paddingTop: 5,
+  },
+  secondContainer: {
+    // flex:1,
+    flexDirection: "row",
+  },
+  dateText: {
+    fontWeight: "bold",
+    textAlign: "right",
+  },
+});
